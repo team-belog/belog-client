17d425d3a08951eb4d2ca63f5b6aec4826e2d731
kimseongyeon <seongyeon0810@naver.com>
2026-09-08 01:05:39 +0900

    feat:  bill-log Step4 컴포넌트 제작

--- src/features/bill-log/components/steps/Step4.tsx
+++ src/features/bill-log/components/steps/Step4.tsx
@@ -5,7 +5,7 @@ interface Step4Props {
 export default function Step4({ onSubmit }: Step4Props) {
   return (
     <div className="flex flex-col">
-      <h1 className="pretendard-sb-20 ml-4 mt-[10px]">Step 4</h1>
+      <h1 className="pretendard-sb-20 ml-4 mt-[10px]">이대로 정산을 요청할까요?</h1>
     </div>
   );
 }
