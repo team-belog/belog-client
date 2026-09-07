56125b9c75a8f385b88072e7e23067088a499f02
Jihyeon Lee <loveljh0109@naver.com>
2026-09-07 17:11:41 +0900

    feat: login 화면 구현

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,21 @@
+import BackHeader from "@/components/layout/BackHeader";
+import Divider from "@/components/layout/Divider";
+import AuthIntro from "@/features/auth/components/AuthIntro";
+import BelogLogo from "@/features/auth/components/BelogLogo";
+import GoogleAuthButton from "@/features/auth/components/GoogleAuthButton";
+import RegisterPrompt from "@/features/auth/components/RegisterPrompt";
+
 export default function LoginPage() {
   return (
-    <main className="p-4">
-      <h1 className="pretendard-sb-20">로그인</h1>
+    <main className="flex flex-1 flex-col items-center pb-[35px]">
+      <BackHeader title="로그인" />
+      <Divider />
+      <div className="flex flex-1 flex-col items-center justify-center gap-[30px]">
+        <BelogLogo />
+        <AuthIntro />
+      </div>
+      <GoogleAuthButton />
+      <RegisterPrompt />
     </main>
   );
 }
