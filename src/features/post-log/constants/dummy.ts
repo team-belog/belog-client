aead811b933db3e468b6c7ff27a805dc8e7c601b
kimseongyeon <seongyeon0810@naver.com>
2026-09-10 13:25:59 +0900

    feat: Post-log 페이지 구현

---
+++ src/features/post-log/constants/dummy.ts
@@ -0,0 +1,17 @@
+export const DUMMY_PHOTO_GROUPS = [
+  {
+    time: "12:00",
+    photos: [
+      { id: 1, src: undefined, likeCount: 0 },
+      { id: 2, src: undefined, likeCount: 3 },
+    ],
+  },
+  {
+    time: "14:30",
+    photos: [
+      { id: 3, src: undefined, likeCount: 0 },
+      { id: 4, src: undefined, likeCount: 3 },
+      { id: 5, src: undefined, likeCount: 0 },
+    ],
+  },
+];
