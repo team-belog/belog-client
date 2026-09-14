cd0e39d59642a218a688d516012105502ea54463
Jihyeon Lee <loveljh0109@naver.com>
2026-09-14 18:00:59 +0900

    fix: FAB별 라우팅 주소 변경

--- src/features/home/components/HomeFab.tsx
+++ src/features/home/components/HomeFab.tsx
@@ -8,12 +8,12 @@ import BelogIcon from "@/components/ui/BelogIcon";
 const MENU_ITEMS = [
   {
     label: "그룹 생성하기",
-    href: "/pre-log",
+    href: "/group/new",
     highlighted: true,
   },
   {
     label: "초대코드로 참여",
-    href: "/post-log",
+    href: "/group/join",
     highlighted: false,
   },
 ] as const;
